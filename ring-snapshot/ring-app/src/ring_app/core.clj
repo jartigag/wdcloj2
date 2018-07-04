319ce5efce3d751f9554caeb8d15ad84d5766063
jartigag <jartigag@protonmail.com>
2018-07-04 20:12:04 +0200

    ring snapshot

---
+++ ring-snapshot/ring-app/src/ring_app/core.clj
@@ -0,0 +1,15 @@
+(ns ring-app.core
+	(:require [ring.adapter.jetty :as jetty]
+			  [ring.util.response :as response]))
+
+(defn handler [request-map]
+	(response/response
+		(str "<html><body> your IP is: "
+	 			(:remote-addr request-map)
+				 "</body></html>")))
+
+(defn -main []
+	(jetty/run-jetty
+		handler
+		{:port 3000
+		 :join? false}))
