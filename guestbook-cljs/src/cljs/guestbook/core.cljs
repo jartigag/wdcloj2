6ebc97f75ae805812e5c17b2c37d622cb7bb1383
jartigag <jartigag@protonmail.com>
2018-07-14 01:07:01 +0200

    wip: clojurescript

---
+++ guestbook-cljs/src/cljs/guestbook/core.cljs
@@ -0,0 +1,33 @@
+(ns guestbook.core
+	(:require [reagent.core :as reagent :refer [atom]]))
+
+(defn message-form []
+  (let [fields (atom {})]
+    (fn []
+      [:div.content
+        [:div.form-group
+        [:p "name:" (:name @fields)]
+          [:p "message:" (:message @fields)]
+        [:p "Name:"
+            [:input.form-control
+              {:type :text
+                :name :name
+                :on-change #(swap! fields assoc :name (-> % .-target .-value))
+                :value (:name @fields)}]]]
+        [:p "Message:"
+          [:textarea.form-control
+            {:rows 4
+             :cols 50
+             :name :message
+             :on-change #(swap! fields assoc :message (-> % .-target .-value))}
+            (:message @fields)]]
+        [:input.btn.btn-primary {:type :submit :value "comment"}]])))
+
+(defn home []
+	[:div.row
+    [:div.span12
+      [message-form]]])
+
+(reagent/render
+[home]
+(.getElementById js/document "content"))
